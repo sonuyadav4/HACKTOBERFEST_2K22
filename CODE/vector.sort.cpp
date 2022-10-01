82b4594364394cd39c7090e8769f2c044c09cd4b
sonu yadav <83870916+sonuyadav2@users.noreply.github.com>
2022-10-01 20:59:42 +0530

    Add files via upload

---
+++ CODE/vector.sort.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+vector<int>v;
+v.push_back(10);
+v.push_back(0);
+v.push_back(12);
+v.push_back(5);
+sort(v.begin(),v.end());
+for(int i=0;i<v.size();i++){
+    cout<<v[i];
+}
+}
